797ed56f615de15d246dc6de175b160eebbf7b60
LizethPatino <lizeth@outlook.com>
2019-01-20 20:12:45 -0500

    Updates

---
+++ precache-manifest.fd7b399b09b4c3ba79cfdd3c48cda204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ec8ab93a5fd242ca0b783b2a31d079d",
+    "url": "/Pokepedia/static/media/tituloPoke.7ec8ab93.png"
+  },
+  {
+    "revision": "24b9da12f5665e7494d1",
+    "url": "/Pokepedia/static/js/runtime~main.24b9da12.js"
+  },
+  {
+    "revision": "51fc381b8d9b722b87f9",
+    "url": "/Pokepedia/static/js/main.51fc381b.chunk.js"
+  },
+  {
+    "revision": "26952face0d3ce63e512",
+    "url": "/Pokepedia/static/js/1.26952fac.chunk.js"
+  },
+  {
+    "revision": "51fc381b8d9b722b87f9",
+    "url": "/Pokepedia/static/css/main.20de6dce.chunk.css"
+  },
+  {
+    "revision": "ac45899a5ad77cd5afcd1a4263163c87",
+    "url": "/Pokepedia/index.html"
+  }
+];
